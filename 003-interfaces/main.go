e2f9204f1fb6857a1824414ddf2911be248586a5
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2021-11-30 11:32:05 -0500

    actualizar

--- 003-interfaces/main.go
+++ 003-interfaces/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/Amataca/practices/003-interfaces/users"
+import (
+	"fmt"
+
+	"github.com/Amataca/practices/003-interfaces/users"
+)
 
 func main() {
 	frank := users.NewEmployee("Frank")
-	_ = frank
+	fmt.Println(frank)
 }
--- 003-interfaces/users/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Amataca/practices/003-interfaces/users
-
-go 1.17
