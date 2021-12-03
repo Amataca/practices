5cc6a5de88773c6ceece8b2a6ec51178472b559d
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2021-12-03 15:12:49 -0500

    Entendiendo de midleware

--- 006-interfaces2/main.go
+++ 006-interfaces2/main.go
@@ -26,36 +26,56 @@ type hombre struct {
 	respirando bool
 	pensando   bool
 	comiendo   bool
+	esHombre   bool
 }
 
+//hereda propiedades de hombre
 type mujer struct {
-	edad       int
-	altura     float32
-	peso       float32
-	respirando bool
-	pensando   bool
-	comiendo   bool
+	hombre
 }
 
-func (h *hombre) respirar()    { h.respirando = true }
-func (h *hombre) comer()       { h.comiendo = true }
-func (h *hombre) pensar()      { h.pensando = true }
-func (h *hombre) sexo() string { return "Hombre" }
-
-func (m *mujer) respirar()    { m.respirando = true }
-func (m *mujer) comer()       { m.comiendo = true }
-func (m *mujer) pensar()      { m.pensando = true }
-func (m *mujer) sexo() string { return "Mujer" }
-
+func (h *hombre) respirar() { h.respirando = true }
+func (h *hombre) comer()    { h.comiendo = true }
+func (h *hombre) pensar()   { h.pensando = true }
+func (h *hombre) sexo() string {
+	if h.esHombre == true {
+		return "Hombre"
+	} else {
+		return "Mujer"
+	}
+}
 func HumanosRespirando(hu humano) {
 	hu.respirar()
 	fmt.Printf("Soy un/a %s, y ya estoy respirando \n", hu.sexo())
 }
 
-func main() {
-	Pedro := new(hombre)
-	HumanosRespirando(Pedro)
+type perro struct {
+	respirando bool
+	comiendo   bool
+	carnivoro  bool
+}
 
-	Maria := new(mujer)
-	HumanosRespirando(Maria)
+func AnimalesRespirar(an animal) {
+	an.respirar()
+	fmt.Println("Soy un animal y estoy respirando")
+}
+
+func AnimalesCarnivoros(an animal) int {
+	if an.EsCarnivoro() == true {
+		return 1
+	}
+	return 0
+}
+
+func (p *perro) respirar()         { p.respirando = true }
+func (p *perro) comer()            { p.comiendo = true }
+func (p *perro) EsCarnivoro() bool { return p.carnivoro }
+
+func main() {
+	totalCarnivoros := 0
+	Dogo := new(perro)
+	Dogo.carnivoro = true
+	AnimalesRespirar(Dogo)
+	totalCarnivoros += AnimalesCarnivoros(Dogo)
+	fmt.Printf("Total de carnivoros: %d", totalCarnivoros)
 }
