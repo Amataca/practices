31c04b6035d2bcc20bdad0c366782bff9329e29f
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2022-01-11 10:42:12 -0500

    entendiendo de punteros

--- 014-puntero/main.go
+++ 014-puntero/main.go
@@ -6,18 +6,28 @@ type mitipo string
 
 var Alias mitipo
 
-/* type Usuario struct {
-	Nombre *mitipo
-} */
-
-func saludar(n *mitipo) {
-	fmt.Printf("Hola soy %T y tengo 24 años", n)
-}
-
 func main() {
 	Alias = "Amataca"
 	/* mipersona := Usuario{Nombre: "Carlos"} */
-	saludar(&Alias)
-	/* fmt.Println(mipersona) */
+	fmt.Println(Alias) //Normal
 
+	CambiarSinPuntero(Alias)
+	fmt.Println(Alias)
+
+	//Con punteros
+	CambiarConPuntero(&Alias) //se ejuta dentro de la funcion el alias modificado
+	fmt.Println(Alias)
+
+}
+
+func CambiarSinPuntero(n mitipo) {
+	n = "Pinino" //se pone n, para nuevo valor de memoria
+	/* Alias = "Pinino" //cambia el valor, del contexto superior en su respectiva memoria */
+	fmt.Println(n)
+}
+
+func CambiarConPuntero(n *mitipo) {
+	Alias = "Costec"
+	fmt.Println(*n) //muestra lo que contiene el valor de memoria
+	fmt.Println(n)  //muestra el nombre de la memoria
 }
--- 015-structs/main.go
+++ 015-structs/main.go
@@ -6,7 +6,7 @@ type Car struct {
 	Model  int
 	Color  string
 	Engine CarEngine
-	Line   Line
+	Line   *Line
 }
 
 type Line struct {
@@ -25,13 +25,55 @@ func main() {
 		Engine: CarEngine{
 			Version: 8,
 		},
-		Line: Line{
+		Line: &Line{
 			LineName: "trend line",
 		},
 	}
+	car3 := r(2020, "azul", 10)
+	car4 := rr(2021, "azul", 11)
 
 	fmt.Printf("%v", car)
 	fmt.Println()
 	fmt.Printf("%v", car2)
+	fmt.Println()
+	fmt.Printf("%v", car3)
+	fmt.Println()
+	fmt.Printf("%v", car4)
 
+	car3 = car2
+	car5 := &car3
+	/* car3 = car2 */
+	fmt.Println()
+	fmt.Printf("%v", car5)
+	/* fmt.Println()
+	fmt.Printf("%v", &car5) */
+	fmt.Println()
+	fmt.Printf("%v", car3)
+
+}
+
+func r(m int, c string, e int) Car {
+	return Car{
+		Model: m,
+		Color: c,
+		Engine: CarEngine{
+			Version: e,
+		},
+		Line: &Line{
+			LineName: "trend line",
+		},
+	}
+}
+
+func rr(m int, c string, e int) *Car {
+	return &Car{
+		Model: m,
+		Color: c,
+		Engine: CarEngine{
+			Version: e,
+		},
+		Line: &Line{
+			LineName: "trend line",
+		},
+	}
 }
